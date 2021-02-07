05a8ad231d46229e44189cd4adda84a878716074
prachi15091999 <18ucs056@lnmiit.ac.in>
2021-02-07 11:05:50 +0530

    A

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn postweb.wsgi  --log-file -
---
+++ profiles/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path("",views.index,name="Profilehome"),
+    path("about/",views.about,name="AboutUs"),
+    path("contact/",views.contact,name="ContactUs"),
+    path("tracker/",views.tracker,name="Tracker"),
+    path("search/",views.search,name="Search"),
+    path("products/<int:myid>",views.product,name="Productview"),
+    path("checkout/",views.checkout,name="Checkout"),
+    path("handlerequest/", views.handlerequest, name="HandleRequest"),
+
+    
+]
+
+
